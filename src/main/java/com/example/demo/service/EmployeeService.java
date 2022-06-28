2b89057f08060082f81d2548689c78fd65c6eaa5
Ozodbek <ozodbek.xomidbekov@gmail.com>
2022-06-28 11:01:03 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.model.Employee;
+import com.example.demo.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+//        isEmailUnique(employee);
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee udpateEmployee(Employee employee) {
+//       isEmailUnique(employee);
+        return employeeRepo.save(employee);
+    }
+
+
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+    private void isEmailUnique(Employee employee) {
+        Optional<Employee> employeeByEmail = employeeRepo.findEmployeeByEmail(employee.getEmail());
+        if (employeeByEmail.isPresent()){
+            throw  new IllegalStateException("Email taken");
+        }
+    }
+
+
+}
